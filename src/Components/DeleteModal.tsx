d88a6c7dfc9320e8407a692fde6f28dee426063f
Md Imran Hossen <freelancerimran81@gmail.com>
2023-01-18 14:08:10 +0600

    update

--- src/Components/DeleteModal.tsx
+++ src/Components/DeleteModal.tsx
@@ -8,15 +8,19 @@ const DeleteModal: React.FC<IMyProps> = ({ _id, setOpenDeleteModal }) => {
 
 
     // delete product from database by axios request
-    const handleDelete = async () => {
-        try {
-            const res = await axios.delete(`http://localhost:9000/products/${_id}`);
-            console.log(res);
-            setOpenDeleteModal(false);
-        } catch (error) {
-            console.log(error);
-        }
+    const handleDelete = async (_id:any ) => {
+      
+        fetch(`http://localhost:9000/products/${_id}`, {
+            method: 'DELETE',
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                setOpenDeleteModal(false);
+            }
+            )
     }
+
 
 
     return (
@@ -27,7 +31,7 @@ const DeleteModal: React.FC<IMyProps> = ({ _id, setOpenDeleteModal }) => {
                 <div>
                     <h3 className="text-lg font-bold text-center">Are you sure you want to delete this product?</h3>
                     <div className='flex justify-center items-center gap-3 mt-5'>
-                        <button onClick={() => handleDelete()} className='btn-error rounded-md btn-sm font-bold text-white'>Yes</button>
+                        <button onClick={() => handleDelete(_id)} className='btn-error rounded-md btn-sm font-bold text-white'>Yes</button>
                         <label onClick={() => setOpenDeleteModal(false)} className='btn btn-success font-bold text-white rounded-md btn-sm'>No</label>
 
                     </div>
